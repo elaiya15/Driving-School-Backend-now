7038e5d4d13e4c3a2c28c84e5d79d6d36877a632
elaiya-Spangles <elaiya.spanglesinfotech@gmail.com>
2025-02-18 10:53:13 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-import  mongoose from "mongoose";
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("Database connection failed", error);
-    process.exit(1);
-  }
-};
-export default  connectDB;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+
+const DbConnection = mongoose.connection;
+
+DbConnection.on("error", (error) => {
+  console.log(`Error: connecting to Mongoose database ${error}`);
+});
+
+export default DbConnection;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 
 import bodyParser from "body-parser";
-import connectDB from "./config/db.js";
+import DbConnection from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
@@ -22,9 +22,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/user", adminRoutes);
 app.use("/api/auth", adminRoutes);
 
-// Database Connection
-connectDB();
-
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  DbConnection.once("open", () => {
+    console.log("Database connected successfully");
+  });
+});
