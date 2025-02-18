ed86136b66a1fd0deb21440a663841fc864fa5e6
elaiya-Spangles <elaiya.spanglesinfotech@gmail.com>
2025-02-18 15:40:54 +0530

    update vercel

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-
 import bodyParser from "body-parser";
 import DbConnection from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
@@ -15,24 +14,15 @@ app.use(cors());
 app.use(express.json()); // Built-in body parser
 
 // Routes
-
 app.get("/", (req, res) => res.send("Server running"));
-
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", adminRoutes);
 app.use("/api/auth", adminRoutes);
 
-// Start Server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-//   DbConnection.once("open", () => {
-//     console.log("Database connected successfully");
-//   });
-// });
-const PORT = process.env.PORT || 5000;
-
+// Connect to Database
 DbConnection.once("open", () => {
-  console.log(`Server running on port ${PORT}`);
   console.log("Database connected successfully");
 });
+
+// Export the app for Vercel (this is the key change)
+export default app;
